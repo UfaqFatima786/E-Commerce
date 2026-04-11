05346bb0acba0c32e33dfc974f5bfc13d17b0034
UfaqFatima786 <u.capricorn19@gmail.com>
2026-04-12 02:32:35 +0500

    bags

---
+++ app.js
@@ -0,0 +1,179 @@
+var allItems = {
+    schoolBags: {
+        cartoonishSchoolBags: {
+            blackCartoonishBag: {
+                name: "School Bag",
+                price: "3000 PKR",
+                description: "A durable school bag for students.",
+                src: "https://bachaaparty.com/cdn/shop/files/Z167969-1.jpg?v=1766991533&width=540",
+            },
+            blueCartoonishBag: {
+                name: "School Bag",
+                price: "2000 PKR",
+                description: "A durable school bag for students.",
+                src: "https://bachaaparty.com/cdn/shop/files/Z167967-1.jpg?v=1766991596&width=540",
+            },
+            purpleCartoonishBag: {
+                name: "School Bag",
+                price: "2000 PKR",
+                description: "A durable school bag for students.",
+                src: "https://bachaaparty.com/cdn/shop/files/Z146950.jpg?v=1751285902&width=540",
+            },
+            src: "https://bachaaparty.com/cdn/shop/files/z1670600109_2.jpg?v=1767189066&width=540",
+        },
+        twoColorSchoolBags: {
+            yellowGreyishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/z1670600109_2.jpg?v=1767189066&width=540",
+                description: "A durable school bag for students.",
+            },
+             brownishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/z1670580109_2.jpg?v=1767189023&width=540",
+                description: "A durable school bag for students.",
+
+            },
+             BlackPinkishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/z1670590109_2.jpg?v=1767188988&width=540",
+                description: "A durable school bag for students.",
+
+            },
+        },
+         stylishSchoolBags: {
+            redStylishSchoolBag: {
+                name: "School Bag",
+                price: "4500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/BPE_4879_03d5e046-91f3-47e3-a93f-7b105b06357f.jpg?v=1769521181&width=540",
+                description: "A durable school bag for students.",
+            },
+             blueStylishSchoolBag: {
+                name: "School Bag",
+                price: "4000 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/BPE_4884_a383c7f4-d626-4253-bef1-99a94bba269e.jpg?v=1769521214&width=540",
+                description: "A durable school bag for students.",
+            },
+            blueStylishSchoolBag: {
+                name: "School Bag",
+                price: "4000 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/BPE_4884_a383c7f4-d626-4253-bef1-99a94bba269e.jpg?v=1769521214&width=540",
+                description: "A durable school bag for students.",
+            },
+        },
+        simpleSchoolBags: {
+            yellowGreyishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/z1670600109_2.jpg?v=1767189066&width=540",
+                description: "A durable school bag for students.",
+            },
+             brownishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/z1670580109_2.jpg?v=1767189023&width=540",
+                description: "A durable school bag for students.",
+
+            },
+             BlackPinkishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/z1670590109_2.jpg?v=1767188988&width=540",
+                description: "A durable school bag for students.",
+
+            },
+        },
+
+
+rollingSchoolBags: {
+            yellowGreyishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/DSC_5177_d6d70cd1-12b7-467e-8a5a-82b3497e38ad.jpg?v=1772183841&width=540",
+                description: "A durable school bag for students.",
+            },
+             brownishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/z1670580109_2.jpg?v=1767189023&width=540",
+                description: "A durable school bag for students.",
+
+            },
+             BlackPinkishPlainBag: {
+                name: "School Bag",
+                price: "2500 PKR",
+                src: "https://bachaaparty.com/cdn/shop/files/z1670590109_2.jpg?v=1767188988&width=540",
+                description: "A durable school bag for students.",
+
+            },
+        },
+
+
+    },
+
+    ladiesBags: {
+        ladiesSmallHandBags: {
+            purpleSmallHeartBag: {
+                name: "Ladies Hand Bag",
+                price: "2000PKR",
+                src: "https://cdn.shopify.com/s/files/1/2337/7003/files/cropped_image-174116517055675_8609edd5-8075-4ee3-939c-a500100b27e2.jpg?v=1742287157&width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+            blackishHandyBag: {
+                name: "Ladies Hand Bag",
+                price: "1500PKR",
+                src: "https://cdn.shopify.com/s/files/1/2337/7003/files/cropped_image-173201877005333_392acefc-bd46-4842-bba7-982da6dcb20e.jpg?v=1732099086&width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+            brownishHandyBag: {
+                name: "Ladies Hand Bag",
+                price: "1800 PKR",
+                src: "  https://cdn.shopify.com/s/files/1/2337/7003/files/20260406150316-66ed635ea05c4489-media_image-0c4ddac1c9ec4c0897553fe51c32b5f0.jpg?width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+        },
+        ladiesMediumHandBags: {
+            greyDiamondHandyBag: {
+                name: "Ladies Hand Bag",
+                price: "1800PKR",
+                src: "https://cdn.shopify.com/s/files/1/2337/7003/files/20260407102738-96762c718944482b-media_image-434c05a6ec624c87b37356fe8157e5b5.jpg?width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+            brownishHandyBag: {
+                name: "Ladies Hand Bag",
+                price: "3000  PKR",
+                src: "  https://cdn.shopify.com/s/files/1/2337/7003/files/20260406150316-66ed635ea05c4489-media_image-0c4ddac1c9ec4c0897553fe51c32b5f0.jpg?width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+            greyDecentHandyBag: {
+                name: "Ladies Hand Bag",
+                price: "2500 PKR",
+                src: "https://cdn.shopify.com/s/files/1/2337/7003/files/20260406150720-2d20397e185549e3-media_image-afb8498ffe0841eda8eb78e8a2b88185.jpg?width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+        },
+        ladiesLargeHandBags: {
+            blackishHandyBag: {
+                name: "Ladies Hand Bag",
+                price: "3500 PKR",
+                src: "https://cdn.shopify.com/s/files/1/2337/7003/files/20260130070649-6d8cfa4a914c4c8f-media_image-0fb66fb8ac254a0682ed587b9afaeddf.jpg?width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+            greenishHandyBag: {
+                name: "Ladies Hand Bag",
+                price: "4000 PKR",
+                src: "https://cdn.shopify.com/s/files/1/2337/7003/files/20260409092329-7ae677085f2049d6-media_image-e4b51ef6ba1d475182ec88a7beb85315.jpg?width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+            blueishHandyBag: {
+                name: "Ladies Hand Bag",
+                price: "4000 PKR",
+                src: "https://cdn.shopify.com/s/files/1/2337/7003/files/media_image-1f6316c115bc4256a6abd49fce409cf5.webp?width=300&format=webp",
+                description: "A stylish ladies bag for everyday use.",
+            },
+        }
+
+    }
+}
