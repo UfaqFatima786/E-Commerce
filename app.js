981c7623f2d58ab5944226251da1b3da1371dd50
UfaqFatima786 <u.capricorn19@gmail.com>
2026-04-13 01:54:17 +0500

    bags

--- app.js
+++ app.js
@@ -381,3 +381,41 @@ for (var maincategory in allProducts) {
         }
     }
 }
+
+
+// (async () => {
+//   await loadFireflyPreset(tsParticles);
+
+//   await tsParticles.load({
+//     id: "tsparticles",
+//     options: {
+//       preset: "firefly",
+//       background: {
+//         color: "#2b3f97",
+//       },
+//     },
+//   });
+// })();
+
+// tsParticles.load({
+//   id: "tsparticles",
+//   options: {
+//     particles: {
+//       shape: {
+//         type: "square", // starting from v2, this require the square shape script
+//       },
+//     },
+//     background: {
+//         color: "#2b3f97",
+//     },
+//     preset: "firefly",
+//   },
+// });
+
+
+
+
+
+
+
+
